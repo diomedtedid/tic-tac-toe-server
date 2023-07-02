f6862dadfef0b5e0e34bbbf121bafa9f359631e8
Vitalii Proskura <vitalii.proskura@gmail.com>
2023-07-02 08:34:12 +0300

    Player and its mapper

--- src/main/java/org/proskura/tictactoeserver/service/player/PlayerMapper.java
+++ src/main/java/org/proskura/tictactoeserver/service/player/PlayerMapper.java
@@ -1,13 +1,14 @@
 package org.proskura.tictactoeserver.service.player;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 import org.proskura.tictactoeserver.model.player.PlayerEntity;
 import org.proskura.tictactoeserver.model.session.Player;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PlayerMapper {
 
     PlayerMapper INSTANT = Mappers.getMapper(PlayerMapper.class);
